da9cae726422f8208759c264faa3c184754b4280
John Kraczek <john@kraczek.com>
2025-03-10 17:01:52 -0600

    Add FunnelKit Source

--- bin/searchPackagesByVendor.ts
+++ bin/searchPackagesByVendor.ts
@@ -8,7 +8,11 @@ const packagesFilePath = path.join(__dirname, "../data/packages.json");
 
 // Get the vendor prefix from the command line arguments
 const vendorPrefix = process.argv[2] || "ydtb/";
+let filterActive = true;
 
+if (vendorPrefix === "/") {
+  filterActive = false;
+}
 // Read the packages.json file
 fs.readFile(packagesFilePath, "utf8", (err, data) => {
   if (err) {
@@ -19,11 +23,18 @@ fs.readFile(packagesFilePath, "utf8", (err, data) => {
   // Parse the JSON data
   const packages = JSON.parse(data);
 
-  // Filter and list packages that start with the vendor prefix
-  const filteredPackages = Object.keys(packages).filter((pkg) =>
-    pkg.startsWith(vendorPrefix)
-  );
+  let filteredPackages;
+
+  if (filterActive) {
+    // Filter and list packages that start with the vendor prefix
+    filteredPackages = Object.keys(packages).filter((pkg) =>
+      pkg.startsWith(vendorPrefix)
+    );
+  } else {
+    // List all packages without filtering
+    filteredPackages = Object.keys(packages);
+  }
 
   // Print the list of filtered packages
-  console.log(filteredPackages.join(" "));
+  console.log(filteredPackages.join("\n"));
 });
